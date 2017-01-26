1ae76938835cd9dd0bda3cb71a87a31faed7cccd
William Stevens <contact@wastevensv.com>
2017-01-26 14:58:37 -0500

    Added neovim support to install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 echo Installing dependencies.
-mkdir -p ~/.vim/autoload/
+mkdir -pv ~/.vim/autoload/
 wget -O ~/.vim/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo Installing dotfiles.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 ln -sfv $DIR/bashrc ~/.bashrc
+
 ln -sfv $DIR/vimrc ~/.vimrc
+mkdir -v ~/.config
+ln -sfv ~/.vim ~/.config/nvim
+ln -sfv ~/.vimrc ~/.config/nvim/init.vim
+
 ln -sfv $DIR/gitconfig ~/.gitconfig
 echo Installed dotfiles.
