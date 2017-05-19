e2db69a469440ff2d278c57c7870fb4e9ebea9db
William Stevens <contact@wastevensv.com>
2017-05-18 20:43:39 -0400

    Added 'polite' replacement of dotfiles.
    Moves old files rather than overwriting.

--- install.sh
+++ install.sh
@@ -5,15 +5,20 @@ wget -O ~/.vim/autoload/plug.vim \
 echo Installing dotfiles.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
-ln -sfv $DIR/bashrc ~/.bashrc
+
+mv -v ~/.bashrc ~/.bashrc.bak
+ln -sv $DIR/bashrc ~/.bashrc
 
 mv -v ~/.emacs.d ~/.emacs.d.bak
-ln -sfv $DIR/emacs.d ~/.emacs.d
+ln -sv $DIR/emacs.d ~/.emacs.d
 
-ln -sfv $DIR/vimrc ~/.vimrc
+mv -v ~/.vimrc ~/.vimrc.bak
+ln -sv $DIR/vimrc ~/.vimrc
+
 mkdir -v ~/.config
 ln -sfv ~/.vim ~/.config/nvim
 ln -sfv ~/.vimrc ~/.config/nvim/init.vim
 
-ln -sfv $DIR/gitconfig ~/.gitconfig
+mv -v ~/.gitconfig ~/.gitconfig.bak
+ln -sv $DIR/gitconfig ~/.gitconfig
 echo Installed dotfiles.
