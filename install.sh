d228769263e78cd76e2f3a627e209a9057715457
Billy Stevens <contact@wasv.me>
2021-10-21 22:06:56 -0400

    Adds option to install dotfiles without symlinks.

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
-for overlay in $@; do
-    $(dirname $0)/stow.sh $overlay 
+case $1 in
+    put|get|link|unlink)
+        ACTION="$1"
+        shift
+        ;;
+    *)
+        ACTION="put"
+        ;;
+esac
+OVERLAYS=${@:-base zsh tmux gpg git vim nvim}
+
+for overlay in $OVERLAYS; do
+    $(dirname $0)/stow.sh $ACTION $overlay 
 done
--- stow.sh
+++ stow.sh
@@ -1,23 +1,36 @@
 #!/bin/env bash
 if [ -z "$1" ]; then
-    echo Usage: ./stow.sh \<overlay\> [target]
+    echo 'Usage: ./stow.sh (put|get|link|unlink) <overlay> [target]'
     exit -1
 fi
+
+ACTION="$1"
 DOTDIR="$(dirname $(realpath -s $0))"
-PACKAGE="$1"
-TARGET="$(realpath ${2:-$HOME})"
+PACKAGE="$2"
+TARGET="$(realpath ${3:-$HOME})"
 
 SOURCE="$DOTDIR/$PACKAGE"
 
 pushd $SOURCE &>/dev/null
-while read -u 10 dir; do
-    ddir=${dir/#dot-/.}
-    echo DIR: $TARGET/$ddir
-    mkdir -pv $TARGET/$ddir
-done 10< <(find -type d -printf '%P\n')
 while read -u 10 file; do
     dfile=${file/#dot-/.}
     echo FILE: $TARGET/$dfile
-    ln -siv $(realpath $file) $TARGET/$dfile
+    case $ACTION in
+        link)
+            mkdir -pv $(dirname $TARGET/$dfile)
+            ln -siv $(realpath $file) $TARGET/$dfile
+            ;;
+        unlink)
+            rm -iv $TARGET/$dfile
+            cp -iv $(realpath $file) $TARGET/$dfile
+            ;;
+        put)
+            mkdir -pv $(dirname $TARGET/$dfile)
+            cp -biv $(realpath $file) $TARGET/$dfile
+            ;;
+        get)
+            cp -v $TARGET/$dfile $(realpath $file)
+            ;;
+    esac
 done 10< <(find -type f -printf '%P\n')
 popd &>/dev/null
